f873e9f0bb5652151d60ea90e7c2e4c06701405e
MrWillCom <mr.will.com@outlook.com>
2021-08-20 13:48:13 +0000

    Implement password to command `import`

--- src/commands/import.js
+++ src/commands/import.js
@@ -1,6 +1,12 @@
 const copyFile = require('../modules/copyFile')
 const _p = require('../functions/path')
 const fs = require('fs')
+const config = require('../functions/config')
+const output = require('../functions/output')
+const sjcl = require('sjcl')
+const readFile = require('../modules/readFile')
+const writeFile = require('../modules/writeFile')
+const inquirer = require('inquirer')
 
 module.exports = (args) => {
     return new Promise((resolve, reject) => {
@@ -15,10 +21,53 @@ module.exports = (args) => {
                 if (!args.params.quiet) console.log(`Imported public key of key pair '${args.keyName}'`)
             }
             if (args.params.private) {
-                await copyFile(args.params.private, privateKeyPath)
-                if (!args.params.quiet) console.log(`Imported private key of key pair '${args.keyName}'`)
+                const currentConfig = await config.get()
+                if (currentConfig['password']['enabled']) {
+                    if (typeof args.params.password != 'undefined') {
+                        try {
+                            if (sjcl.decrypt(args.params.password.toString(), await readFile(_p.passwordValidationDemo(), 'utf8')) === require('../config/passwordValidationDemoData')) {
+                                await writeFile(
+                                    privateKeyPath,
+                                    sjcl.encrypt(args.params.password.toString(), await readFile(args.params.private, 'utf8')),
+                                )
+                                output(args, `Imported private key of key pair '${args.keyName}'`)
+
+                                resolve(args.keyName)
+                            }
+                        } catch (err) {
+                            if (err.message == `ccm: tag doesn't match`) {
+                                reject(require('../functions/err')('Password is incorrect.', { code: 'RSA_CLI:PASSWORD_INCORRECT' }))
+                            } else {
+                                reject(err)
+                            }
+                        }
+                    } else {
+                        inquirer.prompt([{
+                            type: 'password',
+                            name: 'password',
+                            message: 'Password:'
+                        }]).then(async ({ password }) => {
+                            try {
+                                if (sjcl.decrypt(password, await readFile(_p.passwordValidationDemo(), 'utf8')) === require('../config/passwordValidationDemoData')) {
+                                    await writeFile(
+                                        privateKeyPath,
+                                        sjcl.encrypt(password, await readFile(args.params.private, 'utf8')),
+                                    )
+                                    output(args, `Imported private key of key pair '${args.keyName}'`)
+
+                                    resolve(args.keyName)
+                                }
+                            } catch (err) {
+                                if (err.message == `ccm: tag doesn't match`) {
+                                    reject(require('../functions/err')('Password is incorrect.', { code: 'RSA_CLI:PASSWORD_INCORRECT' }))
+                                } else {
+                                    reject(err)
+                                }
+                            }
+                        })
+                    }
+                }
             }
-            resolve(args.keyName)
         }
 
         if (fs.existsSync(keyPairPath)) {
