4a95246327f321fee1a9a3ce04f52a888caad3f1
MrWillCom <mr.will.com@outlook.com>
2021-08-21 13:42:32 +0000

    Update error handler for `getKey`

--- src/functions/getKey.js
+++ src/functions/getKey.js
@@ -6,20 +6,28 @@ const _p = require('../functions/path')
 
 module.exports = (keyName, type, password) => {
     return new Promise((resolve, reject) => {
+        const handleErrorKeyNotExist = (err) => {
+            if (err.code == 'ENOENT') {
+                reject(require('./err')(`Key '${keyName}' doesn't exist.`, { code: 'RSA_CLI:KEY_NOT_EXIST' }))
+            } else {
+                reject(err)
+            }
+        }
+
         if (fs.existsSync(_p.keysDir(keyName))) {
             if (type == 'public') {
-                readFile(_p.key(keyName).public, 'utf8').then((content) => { resolve(content) })
+                readFile(_p.key(keyName).public, 'utf8').then((content) => { resolve(content) }).catch(handleErrorKeyNotExist)
             } else if (type == 'private') {
                 if (password != null) {
                     readFile(_p.key(keyName).private, 'utf8').then((content) => {
                         resolve(sjcl.decrypt(password, content))
-                    })
+                    }).catch(handleErrorKeyNotExist)
                 } else {
-                    readFile(_p.key(keyName).private, 'utf8').then((content) => { resolve(content) })
+                    readFile(_p.key(keyName).private, 'utf8').then((content) => { resolve(content) }).catch(handleErrorKeyNotExist)
                 }
             }
         } else {
-            reject(require('./err')(`Key '${args.keyName}' doesn't exist.`, { code: 'RSA_CLI:KEY_NOT_EXIST' }))
+            reject(require('./err')(`Key '${keyName}' doesn't exist.`, { code: 'RSA_CLI:KEY_NOT_EXIST' }))
         }
     })
 }
