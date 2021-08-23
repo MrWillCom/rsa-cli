ca6a6337562ed4e6f8d9883b5c86e256e2ebcb21
MrWillCom <mr.will.com@outlook.com>
2021-08-23 13:34:58 +0000

    Handle more errors in command `encrypt`

--- src/commands/encrypt.js
+++ src/commands/encrypt.js
@@ -10,9 +10,17 @@ module.exports = (args) => {
             if (args.params.input) {
                 toEncrypt = await readFile(args.params.input)
             } else {
-                toEncrypt = Buffer.from(args.object)
+                try {
+                    toEncrypt = Buffer.from(args.object)
+                } catch (err) {
+                    if (err.code == 'ERR_INVALID_ARG_TYPE') {
+                        reject(require('../functions/err')('Invalid input.', { code: 'RSA_CLI:INVALID_ENCRYPTION_INPUT' }))
+                    }
+                }
             }
+
             const encrypted = crypto.publicEncrypt(key, toEncrypt).toString('base64')
+
             if (!args.params.quiet) console.log(`Encrypted ${args.params.input ? `file ${args.params.input}` : `'${args.object}'`} with key '${args.keyName}':`)
             if (args.params.output) {
                 await writeFile(args.params.output, encrypted)
@@ -21,6 +29,12 @@ module.exports = (args) => {
                 if (!args.params.quiet) console.log(encrypted)
                 resolve(encrypted)
             }
-        }).catch(reject)
+        }).catch((err) => {
+            if (err.code == 'ERR_OSSL_RSA_DATA_TOO_LARGE_FOR_KEY_SIZE') {
+                reject(require('../functions/err')('Data too large for key size.', { code: 'RSA_CLI:DATA_TOO_LARGE_FOR_KEY_SIZE' }))
+            } else {
+                reject(err)
+            }
+        })
     })
 }
