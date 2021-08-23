f04e96e31787686fdd6368632e621b6926dec95c
MrWillCom <mr.will.com@outlook.com>
2021-08-23 13:49:57 +0000

    Handle more errors in command `remove`

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -5,32 +5,36 @@ const removeDirectory = require('../modules/removeDirectory')
 
 module.exports = (args) => {
     return new Promise((resolve, reject) => {
-        if (fs.existsSync(`${_p.key(args.keyName).pair}`)) {
-            const deleteKey = () => {
-                removeDirectory(`${_p.key(args.keyName).pair}`).then(() => {
-                    if (!args.params.quiet) console.log(`Removed key '${args.keyName}'.`)
-                    resolve(args.keyName)
-                })
-            }
-
-            if (args.params.quiet) { 
-                deleteKey()
-            } else {
-                inquirer.prompt([{
-                    type: 'confirm',
-                    name: 'confirm',
-                    message: `Are you sure to remove key '${args.keyName}'?`,
-                    default: false,
-                }]).then((answers) => {
-                    if (answers.confirm === true) {
-                        deleteKey()
-                    } else {
-                        resolve()
-                    }
-                })
-            }
+        if (typeof args.keyName == 'undefined') {
+            reject(require('../functions/err')('No key name provided.', { code: 'RSA_CLI:CANNOT_GENERATE_KEY_WITHOUT_KEY_NAME' }))
         } else {
-            reject(require('../functions/err')(`Key '${args.keyName}' doesn't exist.`, { code: 'RSA_CLI:KEY_NOT_EXIST' }))
+            if (fs.existsSync(`${_p.key(args.keyName).pair}`)) {
+                const deleteKey = () => {
+                    removeDirectory(`${_p.key(args.keyName).pair}`).then(() => {
+                        if (!args.params.quiet) console.log(`Removed key '${args.keyName}'.`)
+                        resolve(args.keyName)
+                    })
+                }
+
+                if (args.params.quiet) {
+                    deleteKey()
+                } else {
+                    inquirer.prompt([{
+                        type: 'confirm',
+                        name: 'confirm',
+                        message: `Are you sure to remove key '${args.keyName}'?`,
+                        default: false,
+                    }]).then((answers) => {
+                        if (answers.confirm === true) {
+                            deleteKey()
+                        } else {
+                            resolve()
+                        }
+                    })
+                }
+            } else {
+                reject(require('../functions/err')(`Key '${args.keyName}' doesn't exist.`, { code: 'RSA_CLI:KEY_NOT_EXIST' }))
+            }
         }
     })
 }
