8d8b6a902dc2ba372cf7af44233366220d6759b2
MrWillCom <mr.will.com@outlook.com>
2021-08-28 13:30:09 +0000

    Use default string when the translation is not completed

--- src/functions/getString.js
+++ src/functions/getString.js
@@ -1,10 +1,20 @@
 const userConfig = require('./userConfig')
+const fs = require('fs')
+const readFile = require('../modules/readFile')
 const replaceAll = require('../modules/replaceAll')
 
 module.exports = async (key, replacementList) => {
     await userConfig.load()
 
-    var str = require(`../i18n/${await userConfig.get()['lang']}`)[key]
+    var str = await (async () => {
+        try {
+            return require(`../i18n/${await userConfig.get()['lang']}`)[key]
+        } catch (err) {
+            if (err.code == 'MODULE_NOT_FOUND') {
+                return require(`../i18n/${require('../config/defaultUserConfig').lang}`)[key]
+            }
+        }
+    })()
 
     if (replacementList) {
         for (const pattern in replacementList) {
